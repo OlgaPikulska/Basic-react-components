04deca1d5ea611f4040613d90c27b82cba46290a
Olga Pikulska <olga.pikulska.ld@gmail.com>
2023-01-15 00:22:24 +0100

    task 1 and 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import user from './1.SocialMediaProfile/user.json';
+import { Profile } from './1.SocialMediaProfile/Profile';
+import data from './2.Statistics/data.json';
+import { Statistics } from './2.Statistics/Statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      < Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
+
   );
 };
